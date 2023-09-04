d2e6647982a5f650978e7066920168edb3ba0e32
QAJOSEP <papat7442@gmail.com>
2023-09-04 09:06:02 -0600

    added lombok and HATEOAS

--- src/main/java/com/exagonal/tasks/domain/model/AdditionalTaskInfo.java
+++ src/main/java/com/exagonal/tasks/domain/model/AdditionalTaskInfo.java
@@ -1,25 +1,13 @@
 package com.exagonal.tasks.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AdditionalTaskInfo {
     private final Long userId;
     private final String userName;
     private final String userEmail;
 
-    public AdditionalTaskInfo(Long userId, String userName, String userEmail) {
-        this.userId = userId;
-        this.userName = userName;
-        this.userEmail = userEmail;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
 }
