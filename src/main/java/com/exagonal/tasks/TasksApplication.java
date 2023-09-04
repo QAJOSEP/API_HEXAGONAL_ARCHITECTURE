9b176896d0e69f7d9fc67e32ba12adf5129159f0
QAJOSEP <papat7442@gmail.com>
2023-09-04 08:50:01 -0600

    upgrade Java 17, Srping boot 3.1.3 v1.1.0

--- src/main/java/com/exagonal/tasks/TasksApplication.java
+++ src/main/java/com/exagonal/tasks/TasksApplication.java
@@ -3,6 +3,7 @@ package com.exagonal.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TasksApplication {
 
